5736c71cfc15b8a8ad106091b80e187bddf7aed7
Freddie Groom <fredgroom@gmail.com>
2018-08-10 16:24:08 +0100

    input working

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,47 +1,81 @@
-import {Component}  from 'react';
+import { Component } from 'react';
 import React from 'react';
 import { withTracker } from 'meteor/react-meteor-data';
- 
+import ReactDOM from 'react-dom';
 import { RSVP } from '../api/rsvp.js';
 import { on } from 'cluster';
 
- 
+
 // App component - represents the whole app
 class App extends Component {
-  getTasks() {
-//     return [
-//       { _id: 1, text: 'something', firstName: 'This is name 1', lastName: 'This is lastname 2', Rsvp: 'yes', DietReq: 'Diet Requirement', Allergy: 'Allergy1', Contact: 'address', Song: '' },
-//       { _id: 2, text: 'something', firstName: 'This is name 1', lastName: 'This is lastname 2', Rsvp: 'yes', DietReq: 'Diet Requirement', Allergy: 'Allergy1', Contact: 'address', Song: '' },
-//       { _id: 3, text: 'something', firstName: 'This is name 1', lastName: 'This is lastname 2', Rsvp: 'yes', DietReq: 'Diet Requirement', Allergy: 'Allergy1', Contact: 'address', Song: '' },
-//     ];
-  }
- 
-  renderRSVP() {
-    return this.props.rsvp.map((rsvp, i) => (
-        <li key={i}>{rsvp.firstName} {rsvp.lastName} {rsvp.Rsvp} {rsvp.DietReq} {rsvp.Allergy} {rsvp.Contact} </li>
-      ));
-    
+    constructor(props) {
+        super(props);
+        // Find the text field via the React ref
+        this.state = { newRSVPValue: '' };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+
     }
-  
- 
-  render() {
-    return (
-      <div className="container"> 
-        <header>
-          <h1>Todo List</h1>
-        </header>
- 
-        <ul>
-          {this.renderRSVP()}
-        </ul>
-      </div>
-    );
-  }
+
+    handleChange(event) {
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+
+        this.setState({
+            [name]: value
+        });
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+
+        RSVP.insert({
+            firstName: this.state.firstName,
+            createdAt: new Date(), // current time
+        });
+
+        // Clear form
+        this.setState({ firstName: '' })
+    };
+
+    renderRSVP() {
+        return this.props.rsvp.map((rsvp, i) => (
+            <li key={i}>{rsvp.firstName} {rsvp.lastName} {rsvp.Rsvp} {rsvp.DietReq} {rsvp.Allergy} {rsvp.Contact} </li>
+        ));
+
+    };
+
+
+    render() {
+        return (
+            <div className="container">
+                <header>
+                    <h1>Todo List</h1>
+                </header>
+
+                <form className="new-rsvp" onSubmit={this.handleSubmit.bind(this)} >
+                    <input
+                        name="firstName"
+                        type="text"
+                        ref="textInput"
+                        placeholder="new rsvp"
+                        onChange={this.handleChange}
+                        value={this.state.firstName}
+                    />
+                </form>
+
+                <ul>
+                    {this.renderRSVP()}
+                </ul>
+            </div>
+        );
+    }
 }
 
 export default withTracker(() => {
     return {
-      rsvp: RSVP.find({}).fetch(),
+        rsvp: RSVP.find({}, { sort: { createdAt: -1 } }).fetch(),
     };
-  })(App);
+})(App);
 
