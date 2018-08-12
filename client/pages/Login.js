155f79377c78f037607a91d1dc9dd91600c859a8
Darragh McAlone <darraghmcalone@hotmail.co.uk>
2018-08-12 16:46:51 +0100

    added login input fields and button

--- client/pages/Login.js
+++ client/pages/Login.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+export default class Login extends Component {
+    render(props) {
+        return (
+            <div>
 
-export default class Login extends Component{
-    render(){
-        return(
-            <div> 
                 <h1> Login </h1>
+                <form className="loginUser" >
+                    <input
+                        name="emailAddress"
+                        type="text"
+                        ref="textInput"
+                        placeholder="email address"
+
+                    />  
+                    <br />   
+                     <input
+                        name="password"
+                        type="text"
+                        ref="textInput"
+                        placeholder="password"
+
+                    />
+                    <br />  
+                    <Button variant="contained" className={props}>
+                        Submit
+                    </Button>
+                </form>
             </div>
-        )
-    }
-}
+                )
+            }
+        }
