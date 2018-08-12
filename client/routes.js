a61c1c659b0cd3cc60a236c6923e68a29af412a7
Darragh McAlone <darraghmcalone@hotmail.co.uk>
2018-08-12 13:59:01 +0100

    route too another page

---
+++ client/routes.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom'
+import { Switch } from 'react-router';
+import { render } from 'react-dom';
+import About from '../client/pages/About'
+import Login from '../client/pages/Login'
+import App from '../imports/ui/App';
+
+Meteor.startup(() => {
+    render(
+        <BrowserRouter>
+
+            <Switch>
+
+                <Route exact path="/rsvp" component={App} />
+
+                <Route path="/login" component={Login} />
+
+                <Route path="/about" component={About} />
+
+             </Switch>
+
+        </BrowserRouter>,
+        document.getElementById('render-target')
+    );
+});
