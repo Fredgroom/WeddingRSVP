79df5678a1545223750083e8b8ec59b4aab4b3cb
Freddie Groom <fredgroom@gmail.com>
2018-08-13 11:35:55 +0100

    monday morning

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -11,7 +11,7 @@ class App extends Component {
     constructor(props) {
         super(props);
         // Find the text field via the React ref
-        this.state = { newRSVPValue: '' };
+        this.state = { firstName: '', lastName: '', attending: '', declined: '', dietReq: ''};
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
 
@@ -32,16 +32,20 @@ class App extends Component {
 
         RSVP.insert({
             firstName: this.state.firstName,
+            lastName: this.state.lastName,
+            attending: this.state.attending,
+            declined: this.state.declined,
+            dietRequirements: this.state.dietReq,
             createdAt: new Date(), // current time
         });
 
         // Clear form
-        this.setState({ firstName: '' })
+        this.setState({ firstName: '', lastName: '', attending: '', declined: '', dietReq: '' })
     };
 
     renderRSVP() {
         return this.props.rsvp.map((rsvp, i) => (
-            <li key={i}>{rsvp.firstName} {rsvp.lastName} {rsvp.Rsvp} {rsvp.DietReq} {rsvp.Allergy} {rsvp.Contact} </li>
+            <li key={i}>{rsvp.firstName} {rsvp.lastName} {rsvp.Rsvp} {rsvp.dietReq} {rsvp.Allergy} {rsvp.Contact} </li>
         ));
 
     };
@@ -51,7 +55,7 @@ class App extends Component {
         return (
             <div className="container">
                 <header>
-                    <h1>Todo List</h1>
+                    <h1>RSVP</h1>
                 </header>
 
                 <form className="new-rsvp" onSubmit={this.handleSubmit.bind(this)} >
@@ -59,11 +63,39 @@ class App extends Component {
                         name="firstName"
                         type="text"
                         ref="textInput"
-                        placeholder="new rsvp"
+                        placeholder="First Name"
                         onChange={this.handleChange}
                         value={this.state.firstName}
-                    />
+                    /><br/>
+                    <input
+                        name="lastName"
+                        type="text"
+                        ref="textInput"
+                        placeholder="Last Name"
+                        onChange={this.handleChange}
+                        value={this.state.lastName}
+                    /><br/>
+                    <div>
+                        <input type="checkbox" name="attending"
+                            value="attending" onChange={this.handleChange}value={this.state.attending}/>
+                        <label for="Accept">Accept</label>
+                        <input type="checkbox" name="declined"
+                            value="declined" onChange={this.handleChange}value={this.state.declined}/>
+                        <label for="Decline">Decline</label>
+                    </div>
+                    {/* Input for Diet Requirements */}
+                    <input
+                        name="dietRequirements"
+                        type="text"
+                        ref="textInput"
+                        placeholder="Diet Requirements?"
+                        onChange={this.handleChange}
+                        value={this.state.dietReq}
+                    /><br/>
+
+                    <button type='submit'>Submit RSVP</button>
                 </form>
+
 
                 <ul>
                     {this.renderRSVP()}
