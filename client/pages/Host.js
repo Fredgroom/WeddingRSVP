fc21b7d8e610c4e6e4f9424d7610ad469ef5f026
Freddie Groom <fredgroom@gmail.com>
2018-08-14 15:24:07 +0100

    specifying data from mongo Host

--- client/pages/Host.js
+++ client/pages/Host.js
@@ -1,45 +1,55 @@
 import React, { Component } from 'react';
-import {RSVP} from '../../imports/api/rsvp';
+import { RSVP } from '../../imports/api/rsvp';
 import { withTracker } from 'meteor/react-meteor-data';
 import { withStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 
 class Host extends Component {
     renderAllRSVP() {
         return this.props.rsvp.map((rsvp, i) => (
 
-            <tr key={i}>
-                <td>{rsvp.firstName}</td>
-                <td>{rsvp.lastName}</td>
-                <td>{rsvp.Rsvp}</td>
-                <td>{rsvp.dietRequirements}</td>
-                <td>{rsvp.allergies}</td>
-                <td>{rsvp.Transport}</td>
-                <td>{rsvp.songNameToDanceTo}</td>
-                <td>{rsvp.songArtistToDanceTo}</td>
-            </tr>
+            <TableRow key={i}>
+                <TableCell>{rsvp.firstName}</TableCell>
+                <TableCell>{rsvp.lastName}</TableCell>
+                <TableCell>{rsvp.Rsvp}</TableCell>
+                <TableCell>{rsvp.dietRequirements}</TableCell>
+                <TableCell>{rsvp.allergies}</TableCell>
+                <TableCell>{rsvp.Transport}</TableCell>
+                <TableCell>{rsvp.songNameToDanceTo}</TableCell>
+                <TableCell>{rsvp.songArtistToDanceTo}</TableCell>
+            </TableRow>
 
         ));
 
     };
 
-    render(){
-        return(
-            <div> 
+    render() {
+        return (
+            <div>
                 <h1> Host </h1>
                 <p>something</p>
-                <table>
-                    <th>First Name</th>
-                    <th>Last Name</th>
-                    <th>RSVP</th>
-                    <th>Dietary Requirements</th>
-                    <th>Allergies</th>
-                    <th>Transport To Venue</th>
-                    <th>Song Name</th>
-                    <th>Artist Name</th>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>First Name</TableCell>
+                            <TableCell>Last Name</TableCell>
+                            <TableCell>RSVP</TableCell>
+                            <TableCell>Dietary Requirements</TableCell>
+                            <TableCell>Allergies</TableCell>
+                            <TableCell>Transport To Venue</TableCell>
+                            <TableCell>Song Name</TableCell>
+                            <TableCell>Artist Name</TableCell>
+                        </TableRow>
+                    </TableHead>
                     {this.renderAllRSVP()}
-                </table>
+                </Table>
             </div>
-            
+
         )
     }
 }
