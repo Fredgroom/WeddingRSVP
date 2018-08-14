f34ea18a0c0b2d922fdadf310f239c068a680635
Darragh McAlone <darraghmcalone@hotmail.co.uk>
2018-08-14 22:45:10 +0100

    add app nav bar

--- client/layouts/MainLayout.js
+++ client/layouts/MainLayout.js
@@ -1,20 +1,64 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuItem from '@material-ui/core/MenuItem';
 
+const styles = {
+    root: {
+      flexGrow: 1,
+    },
+    flex: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginLeft: -12,
+      marginRight: 20,
+    },
+  };
 const MainLayout = (props) =>
+
     <div className='main-layout'>
         {(props.children)}
-        <li>
-            <Link to="/">Home</Link>
-        </li>
-        <li>
-            <Link to="/about">About</Link>
-        </li>
-        <li>
-            <Link to="/rsvp">Rsvp</Link>
-        </li>
 
+    <AppBar position="static">
+        <Toolbar>
+          <IconButton color="inherit" aria-label="Menu">
+            
+          </IconButton>
 
+          <Typography variant="title" color="inherit" >
+          
+          <Link to="/">
+          <Button color="inherit">
+          Home</Button>
+          </Link>
+          
+          
+          <Link to="/about">
+          <Button color="inherit">
+          About
+          </Button>
+          </Link>
+          
+          <Link to="/rsvp">
+          <Button color="inherit">
+          Rsvp
+          </Button>
+            </Link>
+          
+          </Typography>
+          <Link to="/about">
+          <Button color="inherit">Login</Button>
+          </Link>
+          
+        </Toolbar>
+      </AppBar>
     </div>
 
-export default MainLayout;
+export default withStyles(styles) (MainLayout);
--- client/routes.js
+++ client/routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom'
+import { IndexRoute, BrowserRouter, Route } from 'react-router-dom'
 import { Switch } from 'react-router';
 import { render } from 'react-dom';
 import About from './pages/About'
@@ -13,13 +13,13 @@ Meteor.startup(() => {
 
             <Switch>
 
-                <Route exact path="/rsvp" component={App} />
+                <Route path="/rsvp" component={App} />
 
                 <Route path="/login" component={AccountsUI} />
 
                 <Route path="/about" component={About} />
 
-                <Route path="/" component={MainLayout} />
+                <Route path="/nav" component={MainLayout} />
                
              </Switch>
 
